71783c74bd3f374ce810574713167b1a6dcf1d49
Carlos-Felipe Rodriguez <feliperod0519@hotmail.com>
2024-02-26 14:05:42 -0500

    2024-02-26

---
+++ src/app/globalFeed/components/globalFeed.components.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { FeedComponent } from "../../shared/components/feed/feed.component";
+
+@Component({
+    selector:'mc-global-feed',
+    templateUrl: './globalFeed.component.html',
+    standalone: true,
+    imports : [FeedComponent]
+})
+export class GlobalFeedComponent{
+    apiUrl = '/articles'
+}
---
+++ src/app/globalFeed/globalFeed.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from "@angular/router";
+import { GlobalFeedComponent } from "./components/globalFeed.components";
+
+export const routes: Route[] = [
+    { path: '', component: GlobalFeedComponent }
+]
